7d60b43f38d4d6008767db0664747f62772df7ab
Luis Angel Lopez Alvarez <69830885+LuisSwine@users.noreply.github.com>
2025-06-11 00:46:22 -0600

    Bug registrar usuario fixed

--- trabajot/api/serializers.py
+++ trabajot/api/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from .models import Usuario, Dron, RutasVuelo, Notificaciones, TipoAlerta, EstadoConfirmacion
 
 class UsuarioSerializer(serializers.ModelSerializer):
-    correo = serializers.EmailField(read_only=True)
     class Meta:
         model = Usuario
         fields = ['nombre', 'apellidos', 'correo']
